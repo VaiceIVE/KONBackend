e07ff7e1c153bfdae32f2604db8755202b691510
Isachenko Ivan <vanisachenko@gmail.com>
2023-03-29 20:48:52 +0600

    Again

--- Routers/contentRouter.js
+++ Routers/contentRouter.js
@@ -3,8 +3,8 @@ const Router = require('express');
 const router = new Router();
 const multer = require('multer')();
 
-router.get('/video/:filename', ContentController.getVideoFragment);
-router.get('/audio/:filename', ContentController.getAudioFragment);
+//router.get('/video/:filename', ContentController.getVideoFragment);
+//router.get('/audio/:filename', ContentController.getAudioFragment);
 router.get('/oaoa', (req, res) => {console.log('oaoa'); res.json("ok")});
 router.post('/save', ContentController.saveVideo);
 router.get('/tiflocommented', ContentController.getCommented)
